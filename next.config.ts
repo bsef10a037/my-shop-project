45098284037f8c5b772fa9e93d7a5a395231bb0b
Seemab Bukhari <seemab.bukhari@xprolabs.com>
2026-04-23 00:56:20 +0500

    remove output

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // <--- This is the key!
   images: {
-    unoptimized: true, // Needed for static exports if using the <Image /> component
+    unoptimized: true,
   },
 };
 
